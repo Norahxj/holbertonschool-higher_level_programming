b48a2541046b5a48500f39ffa80d56e042ef03b6
Norahxj <Nhaljuhani@gmail.com>
2026-01-22 15:34:10 +0000

    Fixed the Doc & code of Task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-"""Defines a function that prints a name."""
-
-
 def say_my_name(first_name, last_name=""):
     """Prints My name is <first name> <last name>."""
 
@@ -11,10 +7,10 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     if first_name and last_name:
-        print(f"My name is {first_name} {last_name}")
+        print(f"My name is {first_name} {last_name}", end=" ")
     elif first_name:
-        print(f"My name is {first_name}")
+        print(f"My name is {first_name}", end=" ")
     elif last_name:
-        print(f"My name is {last_name}")
+        print(f"My name is {last_name}", end=" ")
     else:
-        print("My name is")
+        print("My name is", end=" ")
