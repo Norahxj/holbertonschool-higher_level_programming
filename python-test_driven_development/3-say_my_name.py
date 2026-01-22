fb19028847b4e6e424f7c02f806a53693070160c
Norahxj <Nhaljuhani@gmail.com>
2026-01-22 15:51:03 +0000

    Fixed the Doc & code of Task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,6 +9,10 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
+    # Strip any extra spaces just in case
+    first_name = first_name.strip()
+    last_name = last_name.strip()
+
     if first_name and last_name:
         print(f"My name is {first_name} {last_name}")
     elif first_name:
