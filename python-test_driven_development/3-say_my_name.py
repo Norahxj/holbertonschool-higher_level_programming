d5f6182baa46525673e296d7edb66786ef992477
Norahxj <Nhaljuhani@gmail.com>
2026-01-22 16:05:47 +0000

    Fixed the Doc & code of Task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """Defines a function that prints a name."""
 
-def say_my_name(first_name, last_name=""):
-    """Prints My name is <first name> <last name>.
 
-    Raises:
-        TypeError: if first_name or last_name is not a string
-    """
+def say_my_name(first_name, last_name=""):
+    """Prints My name is <first name> <last name>."""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    # Build the name exactly as expected by checker
+    # Construct the full name respecting empty strings
     if first_name and last_name:
-        print(f"My name is {first_name} {last_name}")
+        full_name = f"My name is {first_name} {last_name}"
     elif first_name:
-        print(f"My name is {first_name} ")
+        full_name = f"My name is {first_name} "
     elif last_name:
-        print(f"My name is {last_name} ")
+        full_name = f"My name is {last_name} "
     else:
-        print("My name is ")
+        full_name = "My name is "
+
+    print(full_name)
