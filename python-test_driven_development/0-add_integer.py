878625f330010ae78e8f702bf4176d1f715a8c4b
Norahxj <Nhaljuhani@gmail.com>
2026-01-22 11:45:16 +0000

    Fixed doc & code of Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,16 +12,21 @@ def add_integer(a, b=98):
     Adds two integers.
 
     Floats are cast to integers.
-    Raises TypeError if values cannot be converted.
+    Raises TypeError if arguments are not integers or floats.
     """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
     try:
         a = int(a)
-    except (TypeError, ValueError):
+    except (ValueError, OverflowError):
         raise TypeError("a must be an integer")
 
     try:
         b = int(b)
-    except (TypeError, ValueError):
+    except (ValueError, OverflowError):
         raise TypeError("b must be an integer")
 
     return a + b
