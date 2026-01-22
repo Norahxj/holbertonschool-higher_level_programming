c07718bf0088f0ca220f4aa14d07d03a23fe7e1a
Norahxj <Nhaljuhani@gmail.com>
2026-01-22 16:19:21 +0000

    Added the Doc & code of Task 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Defines a function that prints a square with the character #."""
+"""Defines a function that prints a square with #."""
 
 
-def print_square(size):
-    """
-    Prints a square of `size` using the '#' character.
+def print_square(size=None):
+    """Print a square of size 'size' using the character #.
 
     Args:
-        size (int): The size of the square.
+        size (int): The size of the square
 
     Raises:
         TypeError: if size is not an integer
-        ValueError: if size is less than 0
+        ValueError: if size is < 0
     """
+    if size is None:
+        raise TypeError("print_square() missing 1 required positional argument: 'size'")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
