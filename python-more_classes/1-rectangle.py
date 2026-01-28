12982aa8fc9f5b4df1e0e630108b11fa51d5aa8b
Norahxj <Nhaljuhani@gmail.com>
2026-01-28 08:07:02 +0000

    Fixed the code of Task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -24,6 +24,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle."""
+        return self.__height
+
     @height.setter
     def height(self, value):
         """Sets the height of the rectangle with validation."""
