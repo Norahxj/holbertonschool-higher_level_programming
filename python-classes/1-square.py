a3ef0da7772fa06b3f24913c28d2b1311f0f91cc
Norahxj <Nhaljuhani@gmail.com>
2026-01-28 07:26:34 +0000

    Fixed the code of Task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Module that defines a Square class with a private size attribute."""
 
+
 class Square:
     """Square with a private size"""
 
     def __int__(self, size):
+        """Initializes the square with a private size attribute."""
         self.__size = size
