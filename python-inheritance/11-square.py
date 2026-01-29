9d2fa7aecc2f23cff8ac813f29bf8a8b8e66261e
Norahxj <Nhaljuhani@gmail.com>
2026-01-29 22:43:12 +0000

    Added the doctest of Task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module defines a Square class inheriting from Rectangle."""
+"""defines a Square class inheriting from Rectangle."""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
