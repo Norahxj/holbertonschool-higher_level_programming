ab6dbcb6dc6f7be1dfaee8d8303291b20f7bcfee
Norahxj <Nhaljuhani@gmail.com>
2026-01-29 08:10:32 +0000

    Fixed the code & doc test of Task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-"""Module that defines a MyList class inheriting from list."""
+"""Module that defines a MyList class inheriting from list.
 
-
+Doctests for print_sorted:
+>>> my_list = MyList([1, 4, 2, 3, 5])
+>>> my_list.print_sorted()
+[1, 2, 3, 4, 5]
+>>> my_list = MyList([-3, 0, 2])
+>>> my_list.print_sorted()
+[-3, 0, 2]
+>>> my_list = MyList([])
+>>> my_list.print_sorted()
+[]
+"""
 class MyList(list):
     """Represents a custom list with a method to print it sorted."""
 
