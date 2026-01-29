32e5a321dadf9047ce29a5a42831d1b201ad4b92
Norahxj <Nhaljuhani@gmail.com>
2026-01-29 21:03:03 +0000

    Added the code of Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a BaseGeometry class with an unimplemented area method."""
+
+
+class BaseGeometry:
+    """Represents the base geometry class."""
+
+    def area(self):
+        """Raises an exception indicating area is not implemented."""
+        raise Exception("area() is not implemented")
