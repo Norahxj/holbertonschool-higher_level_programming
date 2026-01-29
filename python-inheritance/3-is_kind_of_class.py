a12cfaa79aeba5040c697ca9ce95814072809a1e
Norahxj <Nhaljuhani@gmail.com>
2026-01-29 08:38:51 +0000

    Fixed the code of Task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Module that defines a function to check if an object is an instance of a class or its subclass."""
+"""Module that defines a function to check if an object is an instance
+of a class or a subclass.
+"""
 
 
 def is_kind_of_class(obj, a_class):
-    """Returns True if obj is an instance of a_class or a subclass, else False."""
+    """Returns True if obj is an instance of a_class or a subclass,
+    else False.
+    """
     return isinstance(obj, a_class)
