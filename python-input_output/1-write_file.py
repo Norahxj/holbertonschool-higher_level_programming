f42f2a2f898eea88c9a72462611dd74c8655420f
Norahxj <Nhaljuhani@gmail.com>
2026-02-02 19:53:21 +0000

    Fixed the code of Task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    """Writes a string to a UTF-8 text file & returns the number of characters."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
