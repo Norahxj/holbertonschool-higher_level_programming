f0c235bdf2becdfcf1d259e3689ad8cfa631ae68
Norahxj <Nhaljuhani@gmail.com>
2026-02-02 19:49:38 +0000

    Fixed the code of Task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that provides a function to write a string to a UTF-8 text file."""
+"""Module provides a function to write a string to a UTF-8 text file."""
 
 
 def write_file(filename="", text=""):
