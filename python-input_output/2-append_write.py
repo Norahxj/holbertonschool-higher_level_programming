e2b7d3868783bbc7ab0bb8b57f89294342690aa9
Norahxj <Nhaljuhani@gmail.com>
2026-02-02 19:58:11 +0000

    Fixed the code of Task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF-8 & returns number of characters."""
+    """Appends a string to end of UTF-8 & returns number of characters."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
