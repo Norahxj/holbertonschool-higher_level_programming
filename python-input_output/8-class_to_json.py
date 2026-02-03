4d0b9caa9326eed6d684f2250a851fd4630fafc2
Norahxj <Nhaljuhani@gmail.com>
2026-02-03 05:29:10 +0000

    Fixed the code of Task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module provides function to convert object to dictionary for JSON serialization."""
+"""provides function to convert object to dictionary for JSON serialization."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description of an object for JSON serialization."""
+    """Returns the dictionary description of object for JSON serialization."""
     return obj.__dict__
