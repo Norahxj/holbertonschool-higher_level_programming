1a8988596c5cf01a8ae724a6f24240c93d4bdf32
Norahxj <Nhaljuhani@gmail.com>
2026-02-03 05:33:49 +0000

    Added the code of Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""defines a Student class with JSON serialization & attribute filtering."""
+
+
+class Student:
+    """Represents a student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attributes contained in this list are included.
+        Otherwise, all attributes are included.
+        """
+        if isinstance(attrs, list):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return self.__dict__.copy()
