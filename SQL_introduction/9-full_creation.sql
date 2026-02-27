c46ee0c1ba0fbc03f3febeff85f324cc0a4f54cf
Norahxj <Nhaljuhani@gmail.com>
2026-02-27 23:04:46 +0000

    Create second_table and add multiple rows. Task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Creates the table second_table and inserts multiple rows
+-- Create second_table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert multiple records
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
+
