4d4e818f826e9f2e22894424f5750265514d1fd8
Norahxj <Nhaljuhani@gmail.com>
2026-02-27 23:50:33 +0000

    Create table force_name with NOT NULL name. Task 3 amended

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,6 @@
--- Creates table force_name with name NOT NULL
+-- Creates the table force_name with id INT and name VARCHAR(256) NOT NULL
+-- If the table exists, do not fail
+
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
