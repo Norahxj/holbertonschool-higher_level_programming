80ec47d84d5e155d02f9029db83a17959dbf1edb
Norahxj <Nhaljuhani@gmail.com>
2026-02-28 00:21:29 +0000

    List genres with number of shows linked, excluding genres with zero shows. Task 13 amended

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
--- Lists genres and number of shows linked, excluding genres with zero shows
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+-- List all genres with the number of shows linked
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
 
