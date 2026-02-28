226ee17fbe34181ebf8135759b4ba79d503066b5
Norahxj <Nhaljuhani@gmail.com>
2026-02-28 07:59:03 +0000

    Lists all cities with their state names. Task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Lists all cities with their state names.
+"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    Lists cities joined with states.
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """)
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
